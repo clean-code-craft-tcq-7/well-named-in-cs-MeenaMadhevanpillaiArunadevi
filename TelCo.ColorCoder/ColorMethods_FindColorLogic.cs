4f489f0de362ab38b434e9a421724774176a0875
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-11-20 01:10:16 +0530

    Create ColorMethods_FindColorLogic.cs

---
+++ TelCo.ColorCoder/ColorMethods_FindColorLogic.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace TelCo.ColorCoder
+{
+    public partial class TelecomColor
+    {
+
+       
+        public static ColorPair FindColorFromPairNumber(int pairNumber)
+        {
+            // The function supports only 1 based index. Pair numbers valid are from 1 to 25
+            int minorSize = colorMapMinor.Length;
+            int majorSize = colorMapMajor.Length;
+            if (pairNumber < 1 || pairNumber > minorSize * majorSize)
+            {
+                throw new ArgumentOutOfRangeException(
+                    string.Format("Argument PairNumber:{0} is outside the allowed range", pairNumber));
+            }
+
+            // Find index of major and minor color from pair number
+            int zeroBasedPairNumber = pairNumber - 1;
+            int majorIndex = zeroBasedPairNumber / minorSize;
+            int minorIndex = zeroBasedPairNumber % minorSize;
+
+            // Construct the return val from the arrays
+            pair = new ColorPair()
+            {
+                majorColor = colorMapMajor[majorIndex],
+                minorColor = colorMapMinor[minorIndex]
+            };
+            return pair;
+        }
+    }
+}
